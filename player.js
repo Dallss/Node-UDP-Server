21dbb2039118c128c24030841e86a5792c7d7c2e
Dallss <raalquicer@up.edu.ph>
2025-04-29 20:41:30 +0800

    turns player.js into a module

--- player.js
+++ player.js
@@ -1,62 +1,42 @@
 import dgram from 'dgram';
-
-let playersPortStart = 1000;
-let offset = 0;
-
-const connect_code = 12345
-const server_host = 'localhost'; // Replace with your server's IP address
-const server_port = 8181;
-  
-const players = {
-    randall: {
-        socket: dgram.createSocket('udp4'),
-        x: 0,
-        y: 0,
-    },
-    Arwin: {
-        socket: dgram.createSocket('udp4'),
-        x: 0,
-        y: 0,
-    },
-    jeric: {
-        socket: dgram.createSocket('udp4'),
-        x: 0,
-        y: 0,
+class player {
+    constructor(name, code){
+        this.connect_code = code || 12345
+        this.socket = dgram.createSocket('udp4')
+        this.name = name
+        this.x = 50
+        this.y = 50
+        
+        this.socket.bind(0,()=>{
+            this.port = this.socket.address().port
+        })
+        this.socket.on('message', (msg, rinfo) => {
+            this.x = msg.readUInt16BE(0)
+            this.y = msg.readUInt16BE(2)
+        });
+        this.socket.on('error', (err) => {
+            console.log(`Client binded on port: ${this.socket.port} error: ${err}.`);
+            this.socket.close();
+        });
     }
 
-}
-function connectPlayer(player){
-    player.socket.on('message', (msg, rinfo) => {
-        player.x = msg.readUInt16BE(1)
-        player.y = msg.readUInt16BE(2)
-
-        console.log(`player: ${Object.keys(player)[0]} x: ${player.x}, y: ${player.y}`)
-    });
-    player.socket.on('error', (err) => {
-        console.log(`Client binded on port: ${player.socket.port} error: ${err}.`);
-        player.socket.close();
-    });
-    player.socket.bind(playersPortStart, () => {
-        console.log(`Client bound to port: ${player.socket.address().port}`);
-
-        const connect_code_buff = Buffer.alloc(2);
-        connect_code_buff.writeUInt16BE(connect_code);
-
-        player.socket.send(connect_code_buff, server_port, server_host, (err) => {
+    connect() {
+        this.socket.connect(8181,'localhost',(err)=>{
             if (err) {
-                console.log(`Error sending message: ${err}`);
+                console.log(`Failed to connect: ${err}`);
             } else {
-                console.log(`Sent connection code to server at ${server_host}:${server_port}`);
+                const connect_code_buff = Buffer.alloc(2);
+                connect_code_buff.writeUInt16BE(this.connect_code);
+                this.socket.send(connect_code_buff, ()=>{
+                    console.log(`Sent connection code to server at localhost 8181`);
+                })
             }
-        });
-    });
-
-    playersPortStart += 1
+        })
+    }
 }
 
+export default player
 
 
-connectPlayer(players.randall)
-connectPlayer(players.Arwin)
-connectPlayer(players.jeric)
+
 
